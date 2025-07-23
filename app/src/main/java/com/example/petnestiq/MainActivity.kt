d34f71794d26e6ba78d37b535eea6a676f848520
宇恒 韩 <hanyuheng123@outlook.com>
2025-07-23 12:03:36 +0800

    界面实现

---
+++ app/src/main/java/com/example/petnestiq/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.petnestiq
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.petnestiq.navigation.MainNavigation
+import com.example.petnestiq.ui.theme.PetNestIQTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PetNestIQTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainNavigation()
+                }
+            }
+        }
+    }
+}
